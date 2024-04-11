a979ff951930113c9d6a4f1b095ddb22b6fb7b16
Erick Rojas <151458346+Shadowkirito123@users.noreply.github.com>
2024-04-11 14:44:37 -0400

    segundo commit

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,13 +1,31 @@
-#crear contraseña aleatoria
+# #crear contraseña aleatoria usando print
+# def crear_contraseña(num):
+#     caracter = 'abcdefghijklm'
+#     num_entero = str(num)
+#     num = int(num_entero[0])
+#     print(num_entero)
+#     print(num_entero[0])
+#     c1 = num - 2
+#     c2 = num
+#     c3 = num -5
+#     contraseña = f'{caracter[c1]}{caracter[c2]}{caracter[c3]}{num * 2}'
+#     print(contraseña)
+# crear_contraseña(9)
+
+#crear contraseña aleatoria usando return
 def crear_contraseña(num):
     caracter = 'abcdefghijklm'
     num_entero = str(num)
     num = int(num_entero[0])
-    print(num_entero)
-    print(num_entero[0])
     c1 = num - 2
     c2 = num
     c3 = num -5
     contraseña = f'{caracter[c1]}{caracter[c2]}{caracter[c3]}{num * 2}'
-    print(contraseña)
-crear_contraseña(9)
+    return contraseña,num
+
+#desempaquetando la funcion con return
+password, primer_numero = crear_contraseña(9)
+
+#mostrar el resultado
+print(f'su contrasena es: {password}')
+print(f'y el numero usado para crearla fue el: {primer_numero}')
